de334fc244c7b592b8d740a2aa4b146559d7d0a6
17hao <sqh1107@gmail.com>
2019-05-14 16:13:26 +0800

    proxy

---
+++ src/typeinfo/Interface.java
@@ -0,0 +1,6 @@
+package typeinfo;
+
+interface Interface {
+    void doSomething();
+    void somethingElse(String arg);
+}
---
+++ src/typeinfo/RealObject.java
@@ -0,0 +1,11 @@
+package typeinfo;
+
+class RealObject implements Interface {
+    public void doSomething() {
+        System.out.println("doSomething");
+    }
+
+    public void somethingElse(String arg) {
+        System.out.println("somethingElse " + arg);
+    }
+}
