e29fe6b3b1682f73f18cd339f630b24f196092ad
17hao <sqh1107@gmail.com>
2019-05-23 19:59:14 +0800

    reusing class

---
+++ src/chapter07_reusing/Cartoon.java
@@ -0,0 +1,24 @@
+package chapter07_reusing;
+
+class Art {
+    Art() {
+        System.out.println("Art constructor");
+    }
+}
+
+class Drawing extends Art {
+    Drawing() {
+        System.out.println("Drawing constructor");
+    }
+}
+
+/* 子类构造器初始化之前会自动调用父类的构造器 */
+public class Cartoon extends Drawing {
+    private Cartoon() {
+        System.out.println("Cartoon constructor");
+    }
+
+    public static void main(String[] args) {
+        new Cartoon();
+    }
+}
