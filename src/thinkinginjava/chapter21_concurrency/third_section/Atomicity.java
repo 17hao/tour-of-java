bc9fb346a8d89d7c220b1c5c9705d445c44045b4
17hao <sqh1107@gmail.com>
2019-06-17 17:27:11 +0800

    share limit source

---
+++ src/thinkinginjava/chapter21_concurrency/third_section/Atomicity.java
@@ -0,0 +1,13 @@
+package thinkinginjava.chapter21_concurrency.third_section;
+
+public class Atomicity {
+    int i;
+
+    void f1() {
+        i++;
+    }
+
+    void f2() {
+        i += 3;
+    }
+}
