bc1567efdd74c7c88d6895820b0b6bd745860af7
17hao <sqh1107@gmail.com>
2019-06-17 17:25:58 +0800

    java generic

---
+++ src/thinkinginjava/chapter15_generics/TupleTest.java
@@ -0,0 +1,22 @@
+package thinkinginjava.chapter15_generics;
+
+class Plane {
+}
+
+public class TupleTest {
+    static TwoTuple<String, Integer> f() {
+        return new TwoTuple<>("hi", 2);
+    }
+
+    static ThreeTuple<String, Integer, Plane> g() {
+        return new ThreeTuple<>("hi", 2, new Plane());
+    }
+
+    public static void main(String[] args) {
+        TwoTuple<String, Integer> ttsi = f();
+        System.out.println(ttsi);
+
+        ThreeTuple<String, Integer, Plane> ttsip = g();
+        System.out.println(ttsip);
+    }
+}
