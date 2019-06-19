768c869d7d9b5d57bcb5f8598998e5bfd0dc787f
17hao <sqh1107@gmail.com>
2019-06-19 15:56:37 +0800

    java is passed by value

---
+++ src/etc/IfDemo.java
@@ -0,0 +1,15 @@
+package etc;
+
+public class IfDemo {
+    static int ifTest(int pre1, int pre2, int n) {
+        if (n == 1)
+            return pre1;
+        if (n == 2)
+            return pre2;
+        else return ifTest(pre2, pre1 + pre2, n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ifTest(0, 1, 2));
+    }
+}
