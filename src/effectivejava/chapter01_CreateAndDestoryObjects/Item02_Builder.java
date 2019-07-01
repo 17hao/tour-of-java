aba546e523d496a905afdb2658ba64c569c92d99
17hao <sqh1107@gmail.com>
2019-07-01 22:58:12 +0800

    effective java item 2

---
+++ src/effectivejava/chapter01_CreateAndDestoryObjects/Item02_Builder.java
@@ -0,0 +1,60 @@
+package effectivejava.chapter01_CreateAndDestoryObjects;
+
+public class Item02_Builder {
+    public static void main(String[] args) {
+        Car smart = new Car.Builder("Benz", "v1").color("green").build();
+        Car customize = new Car.Builder("Benz", "v8").color("red").license(666).build();
+        System.out.println(smart + "\n" + customize);
+    }
+}
+
+/**
+ * Consider builder faced with many constructor parameters.
+ */
+class Car {
+    private final String brand;
+    private final String engine;
+    private final String color;
+    private final int license;
+
+    public static class Builder {
+        // Required parameters
+        private final String brand;
+        private final String engine;
+
+        // Optional parameters
+        private String color = "black";
+        private int license = -1;
+
+        public Builder(String brand, String engine) {
+            this.brand = brand;
+            this.engine = engine;
+        }
+
+        Builder color(String val) {
+            color = val;
+            return this;
+        }
+
+        Builder license(int val) {
+            license = val;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
+
+    private Car(Builder builder) {
+        brand = builder.brand;
+        engine = builder.engine;
+        color = builder.color;
+        license = builder.license;
+    }
+
+    @Override
+    public String toString() {
+        return brand + " " + color + " " + engine + " " + license;
+    }
+}
