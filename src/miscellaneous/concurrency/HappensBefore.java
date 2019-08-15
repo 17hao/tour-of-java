307484c012a2e998042a6f5dbc70d91cebcbeaa2
17hao <sqh1107@gmail.com>
2019-08-15 15:35:52 +0800

    update

--- src/miscellaneous/concurrency/HappensBefore.java
+++ src/miscellaneous/concurrency/HappensBefore.java
@@ -4,13 +4,14 @@ public class HappensBefore {
     private static int x = 0;
     private static int y = 42;
 
-    public static void main(String... args) {
-        x = 1;
+    public static void main(String... args) throws InterruptedException{
         Thread t = new Thread(() -> {
             y = x;
-            System.out.println(y); // output 43 or 1
+            System.out.println("t's y is: " + y);
         });
         t.start();
+        t.join(); // enforce all actions in t happen-before main thread
         x = y + 1;
+        System.out.println("x is: " + x);
     }
 }
