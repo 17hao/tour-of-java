103f2941d47dd513f7d902848730330f09dc7ece
17hao <sqh1107@gmail.com>
2019-08-15 19:23:32 +0800

    double locks singleton

---
+++ src/designpattern/singleton/DoubleLocksSingleton.java
@@ -0,0 +1,18 @@
+package designpattern.singleton;
+
+public class DoubleLocksSingleton {
+    private DoubleLocksSingleton() {}
+
+    private static DoubleLocksSingleton instance = null;
+
+    static DoubleLocksSingleton getInstance(DoubleLocksSingleton instance) {
+        if (instance == null) {
+            synchronized (DoubleLocksSingleton.class) {
+                if (instance == null) {
+                    instance = new DoubleLocksSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
