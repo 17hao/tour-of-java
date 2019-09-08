9f88857a21d1418fb1a7931bcdf7278f5b0716a7
17hao <sqh1107@gmail.com>
2019-09-08 19:35:46 +0800

    update demo of Map<E>

--- src/main/miscellaneous/collection/MapDemo.java
+++ src/main/miscellaneous/collection/MapDemo.java
@@ -1,18 +1,30 @@
 package main.miscellaneous.collection;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class MapDemo {
-    private static Map<String, String> hashMap = new HashMap<>();
+    private Map<String, String> hashMap = new HashMap<>();
+    private Map<String, String> treeMap = new TreeMap<>();
+    private Map<String, String> linkedHashMap = new LinkedHashMap<>();
 
     public static void main(String[] args) {
-        String res = hashMap.put("k1", "v1");
+        MapDemo demo = new MapDemo();
+
+        String res = demo.hashMap.put("k1", "v1");
         System.out.println(res);
-        String newRes = hashMap.put("k1", "new_v1");
+        String newRes = demo.hashMap.put("k1", "new_v1");
         System.out.println(newRes);
-        boolean test = hashMap instanceof TreeMap;
-        System.out.println(test);
+        demo.hashMap.put(null, "null");
+        System.out.println("HashMap: " + demo.hashMap);
+
+        demo.treeMap.put("null", "null"); // It does not permit null keys.
+        demo.treeMap.put("k1", "v1");
+        System.out.println("TreeMap: " + demo.treeMap);
+
+        demo.linkedHashMap.put(null, "null");
+        System.out.println("LinkedHashMap: " + demo.linkedHashMap);
     }
 }
