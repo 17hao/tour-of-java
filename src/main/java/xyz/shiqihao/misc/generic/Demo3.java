53949db6fd5667d9e684a13b25ef9883cc6d3071
17hao <sqh1107@gmail.com>
2019-10-04 20:08:10 +0800

    update

--- src/main/java/xyz/shiqihao/misc/generic/Demo3.java
+++ src/main/java/xyz/shiqihao/misc/generic/Demo3.java
@@ -1,18 +1,19 @@
 package xyz.shiqihao.misc.generic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 通配符的含义是unknown,意味着类型的不确定
  */
 public class Demo3 {
-    void printElement1(List<? extends Number> l) {
+    static void printElement1(List<? extends Number> l) {
         for (Number n : l) {
             System.out.println(n);
         }
     }
 
-    <T> void printElement2(List<T> l) {
+    static <T> void printElement2(List<T> l) {
         for (T t : l) {
             System.out.println(t);
         }
@@ -25,5 +26,12 @@ public class Demo3 {
 
     <T extends Number> void addElement2(List<T> l, T number) {
         l.add(number);
+    }
+
+    public static void main(String[] args) {
+        List<Number> l = new ArrayList<>();
+        l.add(1);
+        l.add(2.0);
+        printElement1(l);
     }
 }
