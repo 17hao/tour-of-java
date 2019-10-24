a53a8d0babbd7eebe767c07e672248ecb70419f2
17hao <sqh1107@gmail.com>
2019-10-24 15:57:28 +0800

    http response

--- src/main/java/xyz/shiqihao/misc/network/HttpResponseAwareViewer.java
+++ src/main/java/xyz/shiqihao/misc/network/HttpResponseAwareViewer.java
@@ -1,6 +1,6 @@
 package xyz.shiqihao.misc.network;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -12,6 +12,21 @@ public class HttpResponseAwareViewer {
             int responseCode = uc.getResponseCode();
             String responseMessage = uc.getResponseMessage();
             System.out.println("HTTP/1.x " + responseCode + " " + responseMessage);
+            for (int i = 1; ; i++) { // It's import that i > 0
+                String headerKey = uc.getHeaderFieldKey(i);
+                String headerValue = uc.getHeaderField(i);
+                if (headerKey == null || headerValue == null) break;
+                System.out.println(headerKey + ": " + headerValue);
+            }
+            System.out.println();
+            try (InputStream is = uc.getInputStream();
+                 Reader reader = new InputStreamReader(is);
+                 Reader bufferedReader = new BufferedReader(reader)) {
+                int ch;
+                while ((ch = bufferedReader.read()) != -1) {
+                    System.out.print((char) ch);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
