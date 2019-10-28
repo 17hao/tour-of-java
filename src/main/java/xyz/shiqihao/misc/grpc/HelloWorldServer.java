98addc430f7d22562e40425b4031d12fa19177f7
17hao <sqh1107@gmail.com>
2019-10-28 21:13:10 +0800

    gRPC server

--- src/main/java/xyz/shiqihao/misc/grpc/HelloWorldServer.java
+++ src/main/java/xyz/shiqihao/misc/grpc/HelloWorldServer.java
@@ -1,9 +1,13 @@
 package xyz.shiqihao.misc.grpc;
 
-import com.google.protobuf.RpcCallback;
-import com.google.protobuf.RpcController;
 import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import xyz.shiqihao.misc.grpc.helloworld.GreeterGrpc;
+import xyz.shiqihao.misc.grpc.helloworld.HelloReply;
+import xyz.shiqihao.misc.grpc.helloworld.HelloRequest;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 public class HelloWorldServer {
@@ -11,8 +15,44 @@ public class HelloWorldServer {
 
     private Server server;
 
-    private void start() {
+    private void start() throws IOException {
         int port = 50051;
+        server = ServerBuilder.forPort(port)
+                .addService(new GreeterImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.err.print("*** Shutting down gRPC server since JVM is shutting down");
+            HelloWorldServer.this.stop();
+            System.err.print("*** Server shut down");
+        }));
     }
 
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    private void awaitUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final HelloWorldServer server = new HelloWorldServer();
+        server.start();
+        server.awaitUntilShutdown();
+    }
+
+    static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+        @Override
+        public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
 }
