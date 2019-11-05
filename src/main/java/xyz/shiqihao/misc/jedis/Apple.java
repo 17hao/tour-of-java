57ae66d30b2896a5d4145e302aca213250af8fe9
17hao <sqh1107@gmail.com>
2019-11-05 20:12:38 +0800

    5 basic data structures in redis

---
+++ src/main/java/xyz/shiqihao/misc/jedis/Apple.java
@@ -0,0 +1,19 @@
+package xyz.shiqihao.misc.jedis;
+
+class Apple {
+    private final String color;
+    private final int weight;
+
+    Apple(String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    String color() {
+        return color;
+    }
+
+    int weight() {
+        return weight;
+    }
+}
---
+++ src/main/java/xyz/shiqihao/misc/jedis/Client_4.java
@@ -0,0 +1,19 @@
+package xyz.shiqihao.misc.jedis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * sadd/spop/smembers
+ */
+public class Client_4 {
+    private static final JedisPool POOL = new JedisPool();
+
+    public static void main(String[] args) {
+        Jedis jedis = POOL.getResource();
+        jedis.sadd("set", "1", "2", "3");
+        System.out.println(jedis.sadd("set", "3")); // output 0
+        System.out.println(jedis.spop("set")); // remove random element
+        System.out.println(jedis.smembers("set"));
+    }
+}
