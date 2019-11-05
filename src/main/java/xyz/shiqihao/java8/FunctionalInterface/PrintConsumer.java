3086ac69a719a5d97444ec87be8a46985e45d49e
17hao <sqh1107@gmail.com>
2019-11-05 09:08:21 +0800

    functional interface Consumer

---
+++ src/main/java/xyz/shiqihao/java8/FunctionalInterface/PrintConsumer.java
@@ -0,0 +1,21 @@
+package xyz.shiqihao.java8.FunctionalInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class PrintConsumer {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        forEach(list, System.out::println);
+    }
+
+    static void forEach(List<Integer> l, Consumer<Integer> c) {
+        for (Integer i : l) {
+            c.accept(i);
+        }
+    }
+}
