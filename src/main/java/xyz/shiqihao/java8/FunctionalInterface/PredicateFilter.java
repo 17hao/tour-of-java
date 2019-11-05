9a29a6d8b8a37995e611e2af559316d2a6d4bb05
17hao <sqh1107@gmail.com>
2019-11-05 09:04:08 +0800

    functional interface

---
+++ src/main/java/xyz/shiqihao/java8/FunctionalInterface/PredicateFilter.java
@@ -0,0 +1,27 @@
+package xyz.shiqihao.java8.FunctionalInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateFilter {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        for (Integer i : filter(list, i -> i < 5)) {
+            System.out.println(i);
+        }
+    }
+
+    static List<Integer> filter(List<Integer> l, Predicate<Integer> p) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer i : l) {
+            if (p.test(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
