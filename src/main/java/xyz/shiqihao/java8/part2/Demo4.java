3e2eca88ce23d560b045f2930494fac2d36cdb71
17hao <sqh1107@gmail.com>
2019-11-06 23:08:57 +0800

    Getting stream from a file.

---
+++ src/main/java/xyz/shiqihao/java8/part2/Demo4.java
@@ -0,0 +1,26 @@
+package xyz.shiqihao.java8.part2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Getting stream from file.
+ */
+public class Demo4 {
+    public static void main(String[] args) throws IOException {
+        try (Stream<String> lines = Files.lines(Paths.get("./src/main/resources/input.txt"))) {
+            List<String> result = lines
+                    .flatMap(l -> Arrays.stream(l.split(" ")))
+                    .distinct()
+                    .collect(Collectors.toList());
+            for (String s : result) {
+                System.out.println(s);
+            }
+        }
+    }
+}
