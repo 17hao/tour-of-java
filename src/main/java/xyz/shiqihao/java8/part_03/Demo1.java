73865c564ba119f451d2115c0da208238747e734
17hao <sqh1107@gmail.com>
2019-11-07 20:21:28 +0800

    java 8 optional

---
+++ src/main/java/xyz/shiqihao/java8/part_03/Demo1.java
@@ -0,0 +1,11 @@
+package xyz.shiqihao.java8.part_03;
+
+import java.util.Optional;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Optional<String> op = Optional.empty();
+        String result = op.orElse("null");
+        System.out.println(result);
+    }
+}
