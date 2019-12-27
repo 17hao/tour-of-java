d619db20a042173708bebc77ec4195ddc9c2b3e0
17hao <sqh1107@gmail.com>
2019-12-27 12:40:39 +0800

    servlet 'hello, world' demo

---
+++ src/main/java/xyz/shiqihao/misc/servlet/HelloWorld.java
@@ -0,0 +1,28 @@
+package xyz.shiqihao.misc.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld extends HttpServlet {
+    private String msg;
+
+    @Override
+    public void init() {
+        msg = "hello, world";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain");
+        PrintWriter writer = resp.getWriter();
+        writer.print(msg);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
